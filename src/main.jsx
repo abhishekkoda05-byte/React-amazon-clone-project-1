ca8587e045a243750e51371f3e5bd438c4935dc7
Abhishek koda <abhishekkoda05@gmail.com>
2026-08-29 11:18:14 +0530

    Configure GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/React-amazon-clone-project-1">
       <App />
     </BrowserRouter>
   </StrictMode>,
