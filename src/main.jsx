e5434c6b3b1d8263d69ca2274add5848db1ba24e
Abhishek koda <abhishekkoda05@gmail.com>
2026-08-29 11:45:01 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/React-amazon-clone-project-1">
       <App />
     </BrowserRouter>
   </StrictMode>,
